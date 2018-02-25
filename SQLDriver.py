23c1d4a261f4204342027e5d0a052b47dc8cf8d6
ms <mschultz@hawaii.edu>
2018-02-25 09:11:39 -1000

    continue building runSQL and SQLDriver

--- SQLDriver.py
+++ SQLDriver.py
@@ -14,7 +14,43 @@ class SQLDriver:
     def get_cfg_dict(self):
         print('getting cfg_dict')
 
-    def run_sql(sql, dbname):
+    def parse_cfg(self, clustercfg):
+        print(__file__ +': reading config file "' + clustercfg + '"')
+        file = open(clustercfg)
+        content = file.read()
+        config_array = content.split("\n")
+        # configList = []
+        config_dict = {}
+        for config in config_array:
+            if config:
+                c = config.split("=")
+                # print (c[0] + ' is ' + c[1])
+                config_key = c[0]
+                configValue = c[1]
+                if(('node' in config_key or 'catalog' in config_key) and 'hostname' in config_key):
+                    #print('config_key has node hostname')
+                    nodename = config_key.split(".")[0]
+                    hostname = configValue.split(":")
+                    configIP = hostname[0]
+                    configPort = hostname[1].split("/")[0]
+                    configDb = hostname[1].split("/")[1]
+                    '''print('nodename is ' + nodename)
+                    print('configValue is ' + configValue)
+                    print('configIP is ' + configIP)
+                    print('configPort is ' + configPort)
+                    print('configDb is ' + configDb)'''        
+                    config_dict[nodename + '.port'] = configPort
+                    config_dict[nodename + '.db'] = configDb + '.db'
+                    configValue = configIP
+                config_dict[config_key]=configValue
+            '''#configList.append(c)[1]
+                    print (c[0] + '=' + c[1])
+                    config_dict[c[0]] = c[1]'''
+        print(__file__ +': config file "' + clustercfg + '" read successfully')
+        file.close()
+        return config_dict
+
+    def run_sql(self, sql, dbname):
         print('runDDL.py: executing sql statement ' + sql) 
         try:
             sqlConn = sqlite3.connect(dbname)
--- runSQL.py
+++ runSQL.py
@@ -12,12 +12,17 @@ def main():
     if(len(sys.argv) >= 3):
         print('executing runSQL')
         clustercfg = sys.argv[1]
-        ddlfile = sys.argv[2]
+        node_sql = sys.argv[2]
 
-        sd = SQLDriver.SQLDriver()
-        sd.get_cfg_dict()
+        sql_driver = SQLDriver.SQLDriver()
+        cfg_dict = sql_driver.get_cfg_dict()
+
+        #read ddlfile as a string to be executed as sql
+        with open(node_sql, 'r') as myfile:
+            ddlSQL=myfile.read().replace('\n', '')
+
     else:
-          print(__file__ + ': ERROR need at least 3 arguments to run properly (e.g. \"python3 runDDL.py cluster.cfg plants.sql\"')
+          print(__file__ + ': ERROR need at least 3 arguments to run properly (e.g. \"python3 runSQL.py cluster.cfg books.sql\"')
 
 if __name__ == '__main__':
     main()
